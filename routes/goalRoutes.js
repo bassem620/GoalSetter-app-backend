469ec5b61c57333b4fa463701873eb859ecfd816
Bassem Saeed <bassemsaeed620@gmail.com>
2022-08-29 01:51:05 +0200

    JWT Auth

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
+const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(getGoals).post(setGoal);
+router.route('/').get(protect, getGoals).post(protect, setGoal);
 
-router.route('/:id').patch(updateGoal).delete(deleteGoal);
+router.route('/:id').patch(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
