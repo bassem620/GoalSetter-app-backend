28063a69cd78d29d35d711577a834815fae0dde2
Bassem Saeed <bassemsaeed620@gmail.com>
2022-09-12 14:23:03 +0200

    Controllers edits

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -35,12 +35,11 @@ const updateGoal = asyncHandler (async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id);
-    if(!user){
+    if(!req.user){
         res.status(401);
         throw new Error('User not found');
     }
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401);
         throw new Error('User not authorized');
     }
@@ -60,13 +59,12 @@ const deleteGoal = asyncHandler (async (req, res) => {
         throw new Error('Goal not found');
     }
     // Check for user
-    const user = await User.findById(req.user.id);
-    if(!user){
+    if(!req.user){
         res.status(401);
         throw new Error('User not found');
     }
     // Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401);
         throw new Error('User not authorized');
     }
