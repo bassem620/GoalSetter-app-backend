704cd3649c438997d5338c7ca33f8026ee90ccdd
Bassem Saeed <bassemsaeed620@gmail.com>
2022-09-13 03:42:07 +0200

    CORS

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const colors =  require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware'); 
@@ -10,11 +12,26 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler); 
+
+// Server frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Please set to production');
+    })
+}
 
 app.listen(port, () => console.log(`Server started on ${port}`));
